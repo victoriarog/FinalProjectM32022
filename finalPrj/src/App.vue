75a7b3017986ec684d9bccf08b09010462e6ade0
Vicka <vicrogatinskaya@gmail.com>
2022-11-24 16:55:32 +0100

    some changes

--- finalPrj/src/App.vue
+++ finalPrj/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import Owen from './Owen.vue'
-
+import Owen from "./Owen.vue";
 </script>
 
 <template>
@@ -10,27 +9,25 @@ import Owen from './Owen.vue'
     <h2>See all the legendary Wows</h2>
   </div>
   <div class="page-wrapper">
-  <p class="txt">
-    Owen Cunningham Wilson (born November 18, 1968) is an American actor. 
-    He has had a long association with filmmaker Wes Anderson with whom he shared writing 
-    and acting credits for Bottle Rocket (1996), Rushmore (1998), and The Royal Tenenbaums 
-    (2001), the last of which earned him a nomination for the Academy Award and 
-    BAFTA Award for Best Screenplay. He has also appeared in Anderson's The Life Aquatic
-    with Steve Zissou (2004), The Darjeeling Limited (2007), The Grand Budapest Hotel (2014),
-    and The French Dispatch (2021). Wilson also starred in the Woody Allen romantic comedy Midnight
-    in Paris (2011) as unsatisfied screenwriter Gil Pender, a role which earned him a Golden
-    Globe Award nomination. In 2014 he appeared in Paul Thomas Anderson's Inherent Vice, and 
-    Peter Bogdanovich's She's Funny That Way. <br><br>
-    He also says wow, like a lot.
-  </p>
-  <h2 class="h2t">Let's see:</h2>
+    <p class="txt">
+      Owen Cunningham Wilson (born November 18, 1968) is an American actor. He
+      has had a long association with filmmaker Wes Anderson with whom he shared
+      writing and acting credits for Bottle Rocket (1996), Rushmore (1998), and
+      The Royal Tenenbaums (2001), the last of which earned him a nomination for
+      the Academy Award and BAFTA Award for Best Screenplay. He has also
+      appeared in Anderson's The Life Aquatic with Steve Zissou (2004), The
+      Darjeeling Limited (2007), The Grand Budapest Hotel (2014), and The French
+      Dispatch (2021). Wilson also starred in the Woody Allen romantic comedy
+      Midnight in Paris (2011) as unsatisfied screenwriter Gil Pender, a role
+      which earned him a Golden Globe Award nomination. In 2014 he appeared in
+      Paul Thomas Anderson's Inherent Vice, and Peter Bogdanovich's She's Funny
+      That Way. <br /><br />
+      He also says wow, like a lot.
+    </p>
+    <h2 class="h2t">Let's see:</h2>
     <Suspense>
-      <Owen/>
+      <Owen />
     </Suspense>
   </div>
 </template>
 
-
-
-<style scoped>
-</style>
--- finalPrj/src/Owen.vue
+++ finalPrj/src/Owen.vue
@@ -1,39 +1,39 @@
-
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const response = ref({})
+const response = ref({});
 
-  // adding delay
-  await new Promise(resolve => {
-    setTimeout(() => { 
-      resolve()
-    }, 10)
-  })
-    
-  try {
-    const res = await fetch('https://owen-wilson-wow-api.onrender.com/wows/random?results=10')
-    console.log(res)
-    response.value = (await res.json())
-  } catch (error) {
-    response.value = { answer: 'Error! Could not reach the API. ' + error, image: '' }
-  }
+// adding delay
+await new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 10);
+});
 
-
+try {
+  const res = await fetch(
+    "https://owen-wilson-wow-api.onrender.com/wows/random?results=10"
+  );
+  console.log(res);
+  response.value = await res.json();
+} catch (error) {
+  response.value = {
+    answer: "Error! Could not reach the API. " + error,
+    image: "",
+  };
+}
 </script>
 
 <template>
-    
-    <div v-for="res in response" class="movie">
-  <h3>{{ res.movie }}</h3>
-  <video controls width="400" height="300">
-    <source :src="res.video['480p']" type="video/mp4">
-  </video>
+  <div v-for="res in response" class="movie">
+    <h3>{{ res.movie }}</h3>
+    <video controls width="400" height="300">
+      <source :src="res.video['480p']" type="video/mp4" />
+    </video>
 
-  <div class="desc">
-    <button><h4>Extra info</h4></button>
-    <p>{{res.year}}</p>
+    <div class="desc">
+      <button><h4>Extra info</h4></button>
+      <p>{{ res.year }}</p>
+    </div>
   </div>
-</div>
-
 </template>
